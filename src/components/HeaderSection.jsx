417b0a7c4a6bb9ab5f810000ec21b725a6dc6af1
akshay-ken <akshaybest3120@gmail.com>
2025-08-10 15:13:08 +0530

    style: add header and font styling to it

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,7 +1,18 @@
 export function HeaderSection() {
   return (
     <header>
-      <div></div>
+      <div>
+        <h1 className="text-2xl font-bold text-Gray-950-Text">
+          Social Media Dashboard
+        </h1>
+        <h2 className="text-base font-semibold font-stretch-200% text-Gray-650-Text">
+          Total Followers: 23,004
+        </h2>
+      </div>
+      <div>
+        <p>Dark Mode</p>
+        <label htmlFor=""></label>
+      </div>
     </header>
   );
 }
