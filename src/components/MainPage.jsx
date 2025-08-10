8fdbef5f0304bf323c9cf454626b290188e37003
akshay-ken <akshaybest3120@gmail.com>
2025-08-10 18:44:08 +0530

    style: add styles to socialbox component and used in maingpage.jsx file

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,11 +1,46 @@
 import { HeaderSection } from "./HeaderSection";
 import { SocialBox } from "./SocialBox";
+import iconFb from "../assets/images/icon-facebook.svg";
+import iconInsta from "../assets/images/icon-instagram.svg";
+import iconTw from "../assets/images/icon-twitter.svg";
+import iconYt from "../assets/images/icon-youtube.svg";
 
 export function MainPage() {
   return (
     <main className="p-[4%] md:px-[10%] flex flex-col">
       <HeaderSection />
-      <SocialBox />
+      <SocialBox
+        iconImg={iconFb}
+        userName={"@nathanf"}
+        userNumber={"1987"}
+        countOf={"Followers"}
+        countNumber={"12"}
+        upOrDown={true}
+      />
+      <SocialBox
+        iconImg={iconTw}
+        userName={"@nathanf"}
+        userNumber={"1044"}
+        countOf={"Followers"}
+        countNumber={"99"}
+        upOrDown={true}
+      />
+      <SocialBox
+        iconImg={iconInsta}
+        userName={"@realnathanf"}
+        userNumber={"11k"}
+        countOf={"Followers"}
+        countNumber={"1099"}
+        upOrDown={true}
+      />
+      <SocialBox
+        iconImg={iconYt}
+        userName={"Nathan F."}
+        userNumber={"8239"}
+        countOf={"Subscribers"}
+        countNumber={"144"}
+        upOrDown={false}
+      />
     </main>
   );
 }
--- src/components/SocialBox.jsx
+++ src/components/SocialBox.jsx
@@ -1,7 +1,32 @@
-export function SocialBox() {
+import iconUp from "../assets/images/icon-up.svg";
+import iconDown from "../assets/images/icon-down.svg";
+export function SocialBox({
+  iconImg,
+  upOrDown,
+  userName,
+  userNumber,
+  countOf,
+  countNumber,
+}) {
   return (
-    <section>
-      <p></p>
+    <section className="w-full relative mt-6 flex flex-col justify-center items-center gap-y-6 bg-Card-BG p-6">
+      {/* top border */}
+      <div className="absolute top-0 left-0 right-0 h-2 insta"></div>
+      {/* box content */}
+      <div className="flex flex-row gap-x-4 items-center">
+        <img src={iconImg} className="size-6" alt="" />
+        <p className="text-base text-Gray-650-Text font-semibold">{userName}</p>
+      </div>
+      <p className="text-6xl font-extrabold text-Gray-950-Text">{userNumber}</p>
+      <p className=" uppercase tracking-[0.3rem] -mt-6 text-base font-light text-Gray-650-Text">
+        {countOf}
+      </p>
+      <div className="flex flex-row justify-center items-center">
+        <img src={upOrDown ? iconUp : iconDown} className="w-4 mx-1" alt="" />
+        <p className="font-bold text-Green-text capitalize">
+          {countNumber} today
+        </p>
+      </div>
     </section>
   );
 }
