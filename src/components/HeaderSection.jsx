2f483e31c21b6b0bd5e32e044050ea9cbc91af17
akshay-ken <akshaybest3120@gmail.com>
2025-08-10 15:47:53 +0530

    style: add toggle switch

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,7 +1,7 @@
 export function HeaderSection() {
   return (
-    <header className="flex flex-col md:flex-row">
-      <div>
+    <header className="flex flex-col md:flex-row md:justify-between">
+      <div className="flex flex-col items-start">
         <h1 className="text-2xl font-bold text-Gray-950-Text">
           Social Media Dashboard
         </h1>
@@ -9,9 +9,15 @@ export function HeaderSection() {
           Total Followers: 23,004
         </h2>
       </div>
-      <div>
-        <p>Dark Mode</p>
-        <label htmlFor=""></label>
+      <hr className="md:hidden my-4" />
+      <div className="flex flex-row justify-between md:items-start items-center">
+        <p className="text-base font-semibold text-Gray-650-Text">Dark Mode</p>
+        {/* custom toggle switch */}
+        <label className="relative cursor-pointer w-16 h-8 bg-gray-300 rounded-full">
+          <input type="checkbox" className="sr-only peer" value="" />
+
+          <div className="w-6 absolute top-1 left-1 h-6 rounded-full transition-all bg-white peer-checked:translate-x-8"></div>
+        </label>
       </div>
     </header>
   );
