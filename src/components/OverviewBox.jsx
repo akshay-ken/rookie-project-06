b466064e11cac5bc5f5c01fc3837a53affe4c13e
akshay-ken <akshaybest3120@gmail.com>
2025-08-11 11:00:35 +0530

    style: add dark varient to overviewbox component

--- src/components/OverviewBox.jsx
+++ src/components/OverviewBox.jsx
@@ -7,15 +7,17 @@ export function OverviewBox({ typeOfStat, iconImg, count, up, percent }) {
     "text-Red-text": up == false,
   });
   return (
-    <section className="bg-Card-BG rounded-xl mb-4 p-6 flex flex-col gap-y-4">
+    <section className="bg-Card-BG dark:bg-dark-Card-BG rounded-xl mb-4 p-6 flex flex-col gap-y-4">
       <div className="flex flex-row justify-between items-center">
-        <p className="text-base font-semibold text-Gray-650-Text">
+        <p className="text-base font-semibold text-Gray-650-Text dark:text-dark-Gray-Text">
           {typeOfStat}
         </p>
         <img src={iconImg} className="size-6" alt="" />
       </div>
       <div className="flex flex-row justify-between items-center">
-        <p className="text-3xl font-bold text-Gray-950-Text">{count}</p>
+        <p className="text-3xl font-bold text-Gray-950-Text dark:text-dark-White-Text">
+          {count}
+        </p>
         <div className="flex flex-row items-center">
           <img src={up ? upIcon : downIcon} className="w-4 h-2" alt="" />
           <p className={`text-sm font-bold ${textColor}`}>{percent}</p>
